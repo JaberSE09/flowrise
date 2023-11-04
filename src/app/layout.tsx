f12e5e66cfac607e73cd4938e7f537ba9ac688a4
Sajid Jaber <sajidjaber@yahoo.com>
2023-11-04 15:13:13 -0500

    added meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import type { Metadata, ResolvingMetadata } from 'next'
+import { createClient } from "@/prismicio";
+
 const nunito = Nunito({
   subsets: ["latin"],
   display: "swap",
@@ -12,6 +15,22 @@ const nunitoSans = Nunito_Sans({
   display: "swap",
   variable: "--font-nunito-sans",
 });
+
+
+export async function generateMetadata(): Promise<Metadata> {
+
+  const client = createClient()
+
+  const page =  await client.getSingle("settings")
+  
+  return {
+    title: page.data.site_title || "Flowrise",
+    description: page.data.meta_desc || "flowrise is the best app",
+   openGraph: {
+    images: [page.data.og_image.url || ""]
+   }
+  }
+}
 
 export default function RootLayout({
   children,
